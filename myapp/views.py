5567b774275e8826b4dd4e484a76d8683d627faa
qiaoxiaobin2018 <qxb1461628082@outlook.com>
2019-06-25 18:13:23 +0800

    new file:   .idea/misc.xml 	new file:   .idea/modules.xml 	new file:   .idea/workspace.xml 	new file:   ".idea/\350\275\257\344\273\266\345\267\245\347\250\213.iml" 	new file:   manage.py 	new file:   myapp/__init__.py 	new file:   myapp/__pycache__/__init__.cpython-36.pyc 	new file:   myapp/__pycache__/admin.cpython-36.pyc 	new file:   myapp/__pycache__/models.cpython-36.pyc 	new file:   myapp/__pycache__/views.cpython-36.pyc 	new file:   myapp/admin.py 	new file:   myapp/apps.py 	new file:   myapp/migrations/0001_initial.py 	new file:   myapp/migrations/__init__.py 	new file:   myapp/migrations/__pycache__/0001_initial.cpython-36.pyc 	new file:   myapp/migrations/__pycache__/__init__.cpython-36.pyc 	new file:   myapp/models.py 	new file:   myapp/tests.py 	new file:   myapp/views.py 	new file:   "\350\275\257\344\273\266\345\267\245\347\250\213/__init__.py" 	new file:   "\350\275\257\344\273\266\345\267\245\347\250\213/__pycache__/__init__.cpython-36.pyc" 	new file:   "\350\275\257\344\273\266\345\267\245\347\250\213/__pycache__/settings.cpython-36.pyc" 	new file:   "\350\275\257\344\273\266\345\267\245\347\250\213/__pycache__/urls.cpython-36.pyc" 	new file:   "\350\275\257\344\273\266\345\267\245\347\250\213/settings.py" 	new file:   "\350\275\257\344\273\266\345\267\245\347\250\213/urls.py" 	new file:   "\350\275\257\344\273\266\345\267\245\347\250\213/wsgi.py"

--- myapp/views.py
+++ myapp/views.py
@@ -59,6 +59,7 @@ def login(request):
             user = models.User.objects.get(email = email)
             if user.password == password:
                 #设置session
+                request.session['is_login'] = 1
                 request.session['user_id'] = user.id
                 request.session['user_name'] = user.username
 
@@ -75,16 +76,58 @@ def login(request):
 def register(request):
     '''
 
-    :param request: username,sex,email,num,password
+    :param request: username,email,num,password
     :return:
     '''
+    if request.method == "POST":
+        num = request.POST.get('num',None)
+        sessionCacheData = request.session.get('sessionCacheData',None)
+        if num != sessionCacheData:
+            print('验证码错误！')
+            return HttpResponse(getandReturn.result_format('0', '验证码错误！', None))
+        else:
+            username = request.POST.get('username', None)
+            useremail = request.POST.get('email', None)
+            password = request.POST.get('password', None)
+            if useremail == "" or password == "":
+                print('邮箱和密码不能为空！')
+                return HttpResponse(getandReturn.result_format('0', '邮箱和密码不能为空！', None))
+            else:
+                user = models.User()
+                user.username = username
+                user.email = useremail
+                user.password = password
+                try:
+                    del request.session['sessionCacheData']
+                    user.save()
+                except:
+                    print('注册失败！')
+                    return HttpResponse(getandReturn.result_format('0', '注册失败！', None))
+
+                print('注册成功！')
+                return HttpResponse(getandReturn.result_format('1', '注册成功！', None))
+    else:
+        print('注册失败，请使用POST方法！')
+        return HttpResponse(getandReturn.result_format('0', '注册失败，请使用POST方法！',None))
+
 # 注销
 def logout(request):
     '''
 
-    :param request:
+    :param request:None
     :return:
     '''
+    try:
+        del request.session['is_login']
+        del request.session['user_id']
+        del request.session['user_name']
+    except:
+        print('注销失败！')
+        return HttpResponse(getandReturn.result_format('0', '注销失败！', None))
+
+    print('注销成功！')
+    return HttpResponse(getandReturn.result_format('1', '注销成功！', None))
+
 # 添加收藏
 def addcollect(request):
     '''
@@ -109,10 +152,47 @@ def getAllcollect(request):
 # 添加足迹
 def addFootprint(request):
     '''
-
-    :param request: search_content,url,title
+    POST
+    :param request: url,title
     :return:
     '''
+    if request.method == "POST":
+        try:
+            userid = request.session.get('user_id', None)
+        except:
+            print("用户未登录！")
+            return HttpResponse(getandReturn(0, "用户未登录！", None))
+        url = request.POST.get('url', None)
+        title = request.POST.get('title',None)
+        if url == '' or title == '':
+            print("添加足迹，内容不能为空！")
+            return HttpResponse(getandReturn(0,"添加足迹，内容不能为空！",None))
+        try:
+            ff = models.Footprint2()
+            ff.user_id = userid
+            ff.url = url
+            ff.title = title
+            ff.save()
+            print("添加足迹成功！")
+        except:
+            print("添加足迹失败！")
+            return HttpResponse(getandReturn(0,"添加足迹失败！",None))
+    else:
+        print("请使用POST方法！")
+        return HttpResponse(getandReturn(0, "请使用POST方法！", None))
+
+    #返回所有足迹
+    try:
+        result = models.Footprint2.objects.values().filter(user_id=userid)
+        resultList = list(result)
+        footprints = json.dumps(resultList)
+        print('添加足迹返回信息成功！')
+        return HttpResponse(getandReturn(1, '添加足迹返回信息成功！', footprints))
+    except:
+        print('添加足迹返回信息失败！')
+        return HttpResponse(getandReturn(0, '添加足迹返回信息失败！', None))
+
+
 # 删除足迹
 def delFootprint(request):
     '''
